2cff9e91553ead0800a820a17198e2189b6bfee3
Ahlam <ahlam@Ahlams-MacBook-Pro.local>
2022-02-24 03:28:25 +0300

    removed scaffold model + add devise gem + add home page style

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'main/index'
   resources :devisions
   resources :targets
   resources :teams
   resources :employees
-  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
  root "main#index"
+ 
 #  root :to=> "main#index"
 end
