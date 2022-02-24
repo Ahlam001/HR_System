73a43bf062de44a36c792016546afe77dbb94d97
Ahlam <ahlam@Ahlams-MacBook-Pro.local>
2022-02-24 16:47:43 +0300

    add active stoarge

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,41 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_23_210959) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_24_023843) do
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "devisions", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "manager"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "employees", force: :cascade do |t|
@@ -26,8 +53,8 @@ ActiveRecord::Schema.define(version: 2022_02_23_210959) do
     t.string "job"
     t.integer "salary"
     t.string "employment_status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "team_id"
     t.integer "devision_id"
     t.index ["devision_id"], name: "index_employees_on_devision_id"
@@ -40,8 +67,8 @@ ActiveRecord::Schema.define(version: 2022_02_23_210959) do
     t.date "start_date"
     t.date "finish_date"
     t.string "status"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "team_id"
     t.index ["team_id"], name: "index_targets_on_team_id"
   end
@@ -51,8 +78,8 @@ ActiveRecord::Schema.define(version: 2022_02_23_210959) do
     t.text "description"
     t.text "members"
     t.string "team_lead"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.integer "devision_id"
     t.index ["devision_id"], name: "index_teams_on_devision_id"
   end
@@ -61,12 +88,14 @@ ActiveRecord::Schema.define(version: 2022_02_23_210959) do
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at", precision: 6
-    t.datetime "remember_created_at", precision: 6
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
---
+++ test/controllers/useers_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class UseersControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
