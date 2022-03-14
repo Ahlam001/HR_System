06fab694e26796f062f53f3a511cde6f3129d157
Ahlam <aalmutairi@mrsool.co>
2022-03-14 21:53:05 +0300

    add some test

--- test/models/employee_test.rb
+++ test/models/employee_test.rb
@@ -4,4 +4,24 @@ class EmployeeTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :employees
+  test "employee creates sucessfully" do
+    emp = Employee.new( name:"ahlamm",
+      email: "a@aa.com",
+      job: "sw eng",
+      salary: 1000,
+      employment_status:"full time",
+      devision_id: 1,
+      team_id: 2)
+    p emp  
+    assert emp.save
+  end
+
+  
+  test 'should not accept email' do
+    emp1 = Employee.new(devision_id: 1, salary: 1000, email: "envalid email")
+    p emp1
+    assert_not emp1.save
+  end
+
 end
